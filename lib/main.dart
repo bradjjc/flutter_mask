cc3f45d5533ac5a37dd30df3f574087332236ffd
bradjjc <jichangjung@yahoo.com>
2021-05-05 20:48:11 +0900

    repository 분리 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_mask/model/store_list.dart';
+import 'package:flutter_mask/repository/store_repository.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -28,37 +29,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Store> stores = [];
+  List<Store> stores = [];
+  var isLoading = false;
 
-  var isLoading = true;
-
-  Future fetch() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    var uri = Uri.parse(
-        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
-    var response = await http.get(uri);
-
-    var jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
-
-    var jsonStores = jsonResult['stores'];
-
-    setState(() {
-      stores.clear();
-      jsonStores.forEach((e) {
-        stores.add(Store.fromJson(e));
-      });
-      isLoading = false;
-    });
-    print('fetch완료');
-  }
+  final storeRepository = StoreRepository();
 
   @override
   void initState() {
     super.initState();
-    fetch();
+
+    storeRepository.fetch().then((value) {
+      setState(() {
+        stores = value;
+      });
+    });
   }
 
   @override
@@ -73,7 +57,13 @@ class _MyHomePageState extends State<MyHomePage> {
           actions: [
             IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: fetch,
+              onPressed: () {
+                storeRepository.fetch().then((value) {
+                  setState(() {
+                    stores = value;
+                  });
+                });
+              },
             )
           ],
         ),
